b67cdfa2563f9b38e924af931e81aa96f3091b53
cookiejack15 <cookiejack15@users.noreply.github.com>
2026-01-14 20:52:04 +0100

    Fix: add trailing slash to base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://protoxassassin.github.io',
-  base: '/portfolio',
+  base: '/portfolio/',
   devToolbar: {
     enabled: false
   },
