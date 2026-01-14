b3a36bd2f51a76a74346bbd42f7c9fd2ab2bb948
cookiejack15 <cookiejack15@users.noreply.github.com>
2026-01-14 19:05:40 +0100

    Initial commit - Portfolio Astro

---
+++ portfolio-main/astro.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://protoxassassin.github.io',
+  base: '/portfolio',
+  devToolbar: {
+    enabled: false
+  },
+  vite: {
+    plugins: [tailwindcss()],
+    server: {
+      allowedHosts: ['localhost', '.ngrok.io', '.ngrok-free.app']
+    }
+  }
+});
